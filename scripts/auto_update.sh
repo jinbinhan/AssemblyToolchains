023509bb36dadec54c538ef72a607bb5c980f1c4
jinbinhan <jinbinhan@gmail.com>
2022-10-30 21:44:09 -0600

    add functions

--- scripts/auto_update.sh
+++ scripts/auto_update.sh
@@ -3,7 +3,25 @@
 path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 cd "$path"
 # echo "更新目录:${path}"
-git reset --hard && git pull && chmod -R 750 ./ && chown -R www:www ./
-new_version=$(git describe --tags $(git rev-list --tags --max-count=1))
-echo "update success!"
-echo "The current tag name is ${new_version}"
+# get the local version from local file v.txt
+local_version=$(cat v.txt)
+new_version=$(git describe --tags $(git rev-list --tags --max-count=1)) #get newest version
+#compare the local version and newest version
+if [ "$local_version" != "$new_version" ]; then
+    echo "发现新版本:${new_version}"
+    #ask user to update or not
+    read -p "当前版本为:${local_version} ,是否更新到最新版本:${new_version}?(y/n)" -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        git pull
+        git checkout ${new_version}
+        echo "更新完成"
+        #update the local version
+        echo ${new_version} >v.txt
+    else
+        echo "取消更新"
+    fi
+fi
+
+# git reset --hard && git pull && chmod -R 750 ./ && chown -R www:www ./
+# echo "update success!"
+# echo "The current tag name is ${new_version}"
