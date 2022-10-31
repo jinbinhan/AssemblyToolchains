eb6f1b699c375d31fc3215d4317b897300ea939b
jinbinhan <jinbinhan@gmail.com>
2022-10-30 23:00:34 -0600

    fix auto_update

--- scripts/auto_update.sh
+++ scripts/auto_update.sh
@@ -9,20 +9,20 @@ if [ ! -f "v.txt" ]; then
 fi
 # get the local version from local file v.txt
 local_version=$(cat v.txt)
-new_version=$(git describe --tags $(git rev-list --tags --max-count=1)) #get newest version
+new_version=$(git describe --tags $(git rev-list --tags --max-count=1)) #get newest version from github
 #compare the local version and newest version
 if [ "$local_version" != "$new_version" ]; then
-    echo "发现新版本:${new_version}"
+    # echo "发现新版本:${new_version}"
     #ask user to update or not
     read -p "当前版本为:${local_version} ,是否更新到最新版本:${new_version}?(y/n)" -n 1 -r
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         git pull
         git checkout ${new_version}
-        echo "更新完成"
+        echo "Update to ${new_version} successfully!"
         #update the local version
         echo ${new_version} >v.txt
     else
-        echo "取消更新"
+        echo "Update canceled!"
     fi
 fi
 
