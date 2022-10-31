ce113ecb0386d6199d6f14c5d23d91f9d7c26457
jinbinhan <jinbinhan@gmail.com>
2022-10-30 21:20:31 -0600

    add some thing

--- scripts/auto_update.sh
+++ scripts/auto_update.sh
@@ -5,3 +5,5 @@ cd "$path"
 # echo "更新目录:${path}"
 git reset --hard && git pull && chmod -R 750 ./ && chown -R www:www ./
 echo "update success! The current version is: $(git rev-parse --short HEAD)"
+VERSION=$(curl -sL https://api.github.com/repos/jinbinhan/AssemblyToolchains/releases | jq -r ".[0].name")
+echo $VERSION
