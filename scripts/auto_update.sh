6e379d7aa750b5ad862278709872ff92a6f3333d
jinbinhan <jinbinhan@gmail.com>
2022-10-30 21:03:10 -0600

    add tips about update

--- scripts/auto_update.sh
+++ scripts/auto_update.sh
@@ -2,6 +2,6 @@
 
 path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 cd $path
-echo "更新目录:${path}"
+# echo "更新目录:${path}"
 git reset --hard && git pull && chmod -R 750 ./ && chown -R www:www ./
-echo "更新完成"
+echo "update success! The current version is: $(git rev-parse --short HEAD)"
