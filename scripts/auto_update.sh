137b1ccc5b8a0f28875777764a61a949ff59f203
jinbinhan <jinbinhan@gmail.com>
2022-10-30 21:25:56 -0600

    test

--- scripts/auto_update.sh
+++ scripts/auto_update.sh
@@ -5,5 +5,6 @@ cd "$path"
 # echo "更新目录:${path}"
 git reset --hard && git pull && chmod -R 750 ./ && chown -R www:www ./
 echo "update success! The current version is: $(git rev-parse --short HEAD)"
-VERSION=$(curl -sL https://api.github.com/repos/jinbinhan/AssemblyToolchains/releases | jq -r ".[0].name")
+# VERSION=$(curl -sL https://api.github.com/repos/jinbinhan/AssemblyToolchains/releases | jq -r ".[0].name")
+wget -qO- -t1 -T2 "https://api.github.com/repos/jinbinhan/AssemblyToolchains/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'
 echo $VERSION
