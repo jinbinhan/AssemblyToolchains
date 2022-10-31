72ac62bd898bce400d596e41de95df33d59a6710
jinbinhan <jinbinhan@gmail.com>
2022-10-31 00:09:47 -0600

    modified:   scripts/auto_update.sh

--- scripts/auto_update.sh
+++ scripts/auto_update.sh
@@ -2,7 +2,6 @@
 
 path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 cd "$path"
-# echo "更新目录:${path}"
 #check if v.txt exists or not than create it adn write 0
 if [ ! -f "v.txt" ]; then
     echo "0" >v.txt
@@ -12,7 +11,6 @@ local_version=$(cat v.txt)
 new_version=$(git describe --tags $(git rev-list --tags --max-count=1)) #get newest version from github
 #compare the local version and newest version
 if [ "$local_version" != "$new_version" ]; then
-    # echo "发现新版本:${new_version}"
     #ask user to update or not
     read -p "New version ${new_version} found,do you want to update?(y/n)" -n 1 -r
     if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -25,7 +23,3 @@ if [ "$local_version" != "$new_version" ]; then
         echo "Update canceled!"
     fi
 fi
-
-# git reset --hard && git pull && chmod -R 750 ./ && chown -R www:www ./
-# echo "update success!"
-# echo "The current tag name is ${new_version}"
