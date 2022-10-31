daf7e1c0d340dce30fc44ffd8c69cc8d995d9fb6
jinbinhan <jinbinhan@gmail.com>
2022-10-30 21:04:51 -0600

    fixed a bug

--- scripts/auto_update.sh
+++ scripts/auto_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-cd $path
+cd "$path"
 # echo "更新目录:${path}"
 git reset --hard && git pull && chmod -R 750 ./ && chown -R www:www ./
 echo "update success! The current version is: $(git rev-parse --short HEAD)"
